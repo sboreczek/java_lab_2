ad4c17a149607f01f6396b194962e5fc4d6d0c94
sboreczek <szymih2@gmail.com>
2022-01-05 12:10:57 +0100

    Hello world

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,8 +1,19 @@
 public class Main {
 
+    public static void zad1(){
+
+        int i = 1000;
+        do{
+            if(i%46 == 0)
+                System.out.println("liczba " + i + " jest podzielna przez " + 46);
+            i--;
+        }while(i > 0);
+
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+       zad1();
 
     }
 
