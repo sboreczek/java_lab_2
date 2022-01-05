d743253ae3e09f62464d3f89e230bb3c635c5477
sboreczek <szymih2@gmail.com>
2022-01-05 13:04:35 +0100

    Zadanie 2

--- main/src/Main.java
+++ main/src/Main.java
@@ -12,14 +12,40 @@ public class Main {
     }
 
     public static void zad2(){
-        
+
+        int i = 0;
+        int[] tab = {5,25,64,32,15};
+        while(i<5){
+            System.out.println(tab[i] + " ");
+            i++;
+        }
+
+        i = 0;
+
+        double[] tab2 = {45.43,213.324,22.66,55.123,52.43};
+        while(i<5){
+            System.out.println(tab2[i] + " ");
+            i++;
+        }
+
+        i = 0;
+
+        String[] tab3 = {"KOT","Woda","PRimavera","PAPPARDELLE","nintendo"};
+        while(i<5){
+            System.out.println(tab3[i] + " ");
+            i++;
+        }
+    }
+
+    public static void zad3(){
+
 
 
     }
 
     public static void main(String[] args) {
 
-        zad1();
+        zad2();
 
     }
 
