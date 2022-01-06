24ac039557e52964ca628d7db104b7c4c3f19478
sboreczek <szymih2@gmail.com>
2022-01-06 22:43:28 +0100

    Wszystkie zadania wykonane

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,19 +1,8 @@
 public class Main {
 
-    public static void zad1(){
-
-        int i = 1000;
-        do{
-            if(i%46 == 0)
-                System.out.println("liczba " + i + " jest podzielna przez " + 46);
-            i--;
-        }while(i > 0);
-
-    }
-
     public static void main(String[] args) {
 
-       zad1();
+        System.out.println("Hello World");
 
     }
 
